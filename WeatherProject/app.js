38cc3b844b51a57bb9f48e923143dfdca8863da3
Excel Okechukwu <oexcel77@gmail.com>
2023-04-29 22:42:42 +0100

    node https

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -6,8 +6,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+
+    const url = 'https://random-data-api.com/api/v2/users?size=2&is_xml=true'
+    https.get(url, (response) => {
+        console.log(response)
+    })
+
     res.send('work in progress')
 })
+
+
 
 
 app.listen(port, () => {
