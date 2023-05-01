88d8790f079f728db8e90ab9109bec48eda884af
Excel Okechukwu <oexcel77@gmail.com>
2023-05-01 22:08:36 +0100

    parse json 2

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -11,14 +11,14 @@ app.get('/', (req, res) => {
     https.get(url, (response) => {
         response.on('data', (data) => {
             const randomData = JSON.parse(data)
-            const title = randomData[0].employment.key_skill
-            console.log(title)
+            const title = randomData[0].employment.title
+            const skill = randomData[0].employment.key_skill
+            res.write(`<h1>Your name is ${randomData[0].last_name} ${randomData[0].first_name}<h1/>`)
+            res.write(`<h2>You work as a ${title} and you must have ${skill} capabilities<h2/>`)
+            res.send()
         } )
     })
-
-    res.send('work in progress')
 })
-
 
 
 
